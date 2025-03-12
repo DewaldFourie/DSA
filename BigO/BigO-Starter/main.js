0d216835ecb092aa97421e6850ac4853b84cc976
Dewald <dewaldfourie0808@gmail.com>
2025-03-12 12:36:36 +0200

    added O(1)

--- BigO/BigO-Starter/main.js
+++ BigO/BigO-Starter/main.js
@@ -1,20 +1,40 @@
 
+// This function demonstrates how long it takes for a function to run
+// depending on array size
+
+// this function has a O(n) notation, or linear time
+
 const fish = ['dory', 'bruce', 'marlin', 'nemo'];
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
 const large = new Array(100).fill('nemo');
 
-// This function demonstrates how long it takes for a function to run
-// depending on array size
 function findNemo2(fish) {
-    let t0 = performance.now();
+    console.log('Running O(n) example');
+    console.log('');
+    
     for (let i = 0; i < fish.length; i++) {
         if (fish[i] === 'nemo') {
             console.log('Found NEMO!');
         }
     }
-    let t1 = performance.now();
-    console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
 }
 
-findNemo2(everyone);
+findNemo2(fish);
+
+
+// example 2 
+console.log('');
+console.log('');
+
+const boxes = [0,1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes) { 
+    console.log('Running O(1) example');
+    console.log('');
+
+    console.log(boxes[0]);
+    console.log(boxes[1]);
+}
+
+logFirstTwoBoxes(boxes);

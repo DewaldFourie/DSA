325bf2475b5174902571e615b3d9d046c7528c69
Dewald <dewaldfourie0808@gmail.com>
2025-03-27 15:06:13 +0200

    updated Heaps and Tries

---
+++ DataStructures/Trees/Binary-Heaps/main.js
@@ -0,0 +1,32 @@
+
+// Binary Heap
+
+// used with priority queues 
+
+// Binary Max Heap = Root node is largest, every node below that gets smaller
+
+// Binary Min Heap = Root node is smallest, every node below that gets bigger
+
+
+// Binary Heaps Lookup or search is O(n) , insert & delete is still O(logN)
+// but they are really good for comparative operations
+
+
+// Priority Queue 
+
+// Every element has a certain priority
+// like a nightclub queue, Vip's have a higher priority
+// they get to go in first although they came later
+
+// also think about a hospital emergency room as well
+
+// Pro's
+//
+// PRIORITY
+// Flexible size
+// Fast insert
+
+
+// Con's
+//
+// Slow Lookup
---
+++ DataStructures/Trees/Trie/main.js
@@ -0,0 +1,18 @@
+
+
+// TRIE
+
+// Specialized Tree used in searching
+// Most Often with text
+
+// Depending on the type of search, in most cases it can 
+// outperform hash tables and binary search trees
+
+// Tries are Not a Binary Tree, can have multiple children
+
+// usually starts with an empty START node
+
+// used in autocomplete and word suggestions in real world
+
+// Time Complexity
+// Big O(length of word)

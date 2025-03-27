b02775006e07583d2962862a10c4b951dc87237c
Dewald <dewaldfourie0808@gmail.com>
2025-03-27 16:31:13 +0200

    added Graphs

---
+++ DataStructures/Graphs/main.js
@@ -0,0 +1,17 @@
+
+// ---------- GRAPHS ----------------------------------------
+
+// Trees, Linked-Lists and Heaps are all graphs
+
+// Directed Graphs = one way 
+// Undirected Graphs = can go in opposite direction as well
+
+// Weighted Graphs - if their edges or connection have values 
+// Unweighted Graphs - if their edges doesn't have any values
+
+// Cyclic Graph - when you can complete a cycle(circle) between nodes with edges 
+// Acyclic Graph - when there is no circular connection
+
+//
+//
+

f90eac5ecf549cc9715e855982c4cce803056ba8
Dewald <dewaldfourie0808@gmail.com>
2025-04-01 14:22:11 +0200

    added BFS & DFS

---
+++ Algorithms/Searching/DFS/main.js
@@ -0,0 +1,20 @@
+
+
+// Depth First Search (Traversal)
+
+
+//           9
+//       4       20
+//     1   6  15   170
+//
+
+
+// DFS = [9, 4, 1, 6, 20, 15, 170]
+
+// O(n)
+
+// Pros:
+//      Less Memory
+//      Does path Exists
+// Cons:
+//      can get slow

7708ae6fea3946b0b85e3528ff26084aaec8a7b6
Dewald <dewaldfourie0808@gmail.com>
2025-04-01 11:10:20 +0200

    added linear search

---
+++ Algorithms/Searching/Linear Search/main.js
@@ -0,0 +1,26 @@
+
+
+// Linear Search
+
+// go one by one looking through the list
+
+// best case = O(1)
+// worst case = O(n)
+
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+console.log(beasts.indexOf('Godzilla')); // linear search
+
+console.log(beasts.findIndex(item => item === 'Godzilla'));  // linear search
+
+console.log(beasts.find(item => item === 'Godzilla')); // linear search
+
+console.log(beasts.includes('Godzilla'));  // linear search
+
+// worst case above is that we go through entire list = O(n)
+
+// is there a better way ? what if List was sorted ??
+
+
+
+
